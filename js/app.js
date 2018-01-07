bcd69083288fac514346f604059144ada19ed0d6
bernkaztel <bernkaztel@hotmail.com>
2018-01-07 16:58:41 -0600

    s

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*  Intercambia añadiendo y removiendo la clase responsiva a la navegación cuando el usuario da click*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
